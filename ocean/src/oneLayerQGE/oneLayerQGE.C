ac8f25b625eb8eab57929342ddbfb7645febe95b
Michele <mgirfogl@sissa.it>
2026-04-14 23:54:04 +0200

    changed mail and add header in ocean files

--- ocean/src/oneLayerQGE/oneLayerQGE.C
+++ ocean/src/oneLayerQGE/oneLayerQGE.C
@@ -1,19 +1,26 @@
 /*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-License
-    This file is part of OpenFOAM.
+                           ██████╗ ███████╗ █████╗
+                          ██╔════╝ ██╔════╝██╔══██╗
+                          ██║  ███╗█████╗  ███████║
+                          ██║   ██║██╔══╝  ██╔══██║
+                          ╚██████╔╝███████╗██║  ██║
+                           ╚═════╝ ╚══════╝╚═╝  ╚═╝
 
-    OpenFOAM is free software: you can redistribute it and/or modify it
+                 GEA: Geophysical and Environmental Applications
+
+    Author:        Michele Girfoglio
+    Institution:   University of Palermo
+
+------------------------------------------------------------------------------
+License
+    This file is part of GEA.
+
+    GEA is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    GEA is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
     for more details.
